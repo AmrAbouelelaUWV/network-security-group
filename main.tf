d912bd676b139aaae7b12dcd83f5669d2906b6d1
AmrAbouelelaUWV <Amr.abouelela@uwv.nl>
2026-08-11 11:29:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-resource "azurerm_network_security_group" "this" {
-  name                = var.name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  tags                = var.tags
-}
-
-resource "azurerm_subnet_network_security_group_association" "this" {
-  subnet_id                 = var.subnet_id
-  network_security_group_id = azurerm_network_security_group.this.id
-
-  depends_on = [azurerm_network_security_rule.this]
-}
-
 resource "azurerm_network_security_rule" "this" {
   for_each = { for rule in var.security_rules : rule.name => rule }
 
@@ -20,10 +6,16 @@ resource "azurerm_network_security_rule" "this" {
   direction                   = each.value.direction
   access                      = each.value.access
   protocol                    = each.value.protocol
-  source_port_range           = each.value.source_port_range
-  destination_port_range      = each.value.destination_port_range
+
+  source_port_range           = try(each.value.source_port_range, null)
+  destination_port_range      = try(each.value.destination_port_range, null)
+
+  source_port_ranges          = try(each.value.source_port_ranges, null)
+  destination_port_ranges     = try(each.value.destination_port_ranges, null)
+
   source_address_prefix       = each.value.source_address_prefix
   destination_address_prefix  = each.value.destination_address_prefix
+
   resource_group_name         = var.resource_group_name
   network_security_group_name = azurerm_network_security_group.this.name
 }
