ed308b27a99424378ff8eccf3e452c284ef54f74
AmrAbouelelaUWV <Amr.abouelela@uwv.nl>
2026-08-11 11:30:01 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -22,17 +22,20 @@ variable "tags" {
 
 variable "security_rules" {
   type = list(object({
-    name                        = string
-    priority                    = number
-    direction                   = string
-    access                      = string
-    protocol                    = string
-    source_port_range           = optional(string)
-    destination_port_range      = optional(string)
-    source_port_ranges          = optional(list(string))
-    destination_port_ranges     = optional(list(string))
-    source_address_prefix       = string
-    destination_address_prefix  = string
+    name                       = string
+    priority                   = number
+    direction                  = string
+    access                     = string
+    protocol                   = string
+
+    source_port_range          = optional(string)
+    destination_port_range     = optional(string)
+
+    source_port_ranges         = optional(list(string))
+    destination_port_ranges    = optional(list(string))
+
+    source_address_prefix      = string
+    destination_address_prefix = string
   }))
   default = []
 }
